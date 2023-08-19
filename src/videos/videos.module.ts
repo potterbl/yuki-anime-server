bf7937e1a6a98df2ae32cb7c279caec0667f03cc
potterbl <zeexample42@gmail.com>
2023-08-19 23:52:16 +0300

    updates

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -3,14 +3,14 @@ import {VideosController} from "./videos.controller";
 import {VideosService} from "./videos.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {Video, VideoSchema} from "./schemas/video.schema";
-import {Collection, CollectionSchema} from "../collections/schemas/collection.schema";
+import {Account, AccountSchema} from "../auth/schemas/account.schema";
 
 @Module({
     providers: [VideosService],
     controllers: [VideosController],
     imports: [MongooseModule.forFeature([
         {name: Video.name, schema: VideoSchema},
-        {name: Collection.name, schema: CollectionSchema}
+        {name: Account.name, schema: AccountSchema}
     ])]
 })
 
