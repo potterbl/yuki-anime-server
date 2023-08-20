5e3b3bc4a429d40197620ca29a54352db803b563
potterbl <zeexample42@gmail.com>
2023-08-20 23:53:27 +0300

    videos update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,29 @@ import { AppService } from './app.service';
 import { VideosModule } from './videos/videos.module';
 import { CollectionsModule } from './collections/collections.module';
 import { AuthModule } from './auth/auth.module';
-import {MulterModule} from "@nestjs/platform-express";
+import {MulterModule, MulterModuleOptions} from "@nestjs/platform-express";
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGOOSE_URI),
-      MulterModule.register({
-        dest: './uploads',
-      }),
+    MulterModule.registerAsync({
+      useFactory: () => {
+        const storageOptions: MulterModuleOptions['storage'] = diskStorage({
+          destination: './uploads',
+          filename: (_, file, callback) => {
+            const name = file.originalname.split('.')[0];
+            const fileExtName = extname(file.originalname);
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+            callback(null, `${name}-${uniqueSuffix}${fileExtName}`);
+          },
+        });
+        return {
+          storage: storageOptions,
+        };
+      },
+    }),
     CollectionsModule,
     VideosModule,
     AuthModule,
