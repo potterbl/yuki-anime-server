81c7383c421b7627052c04a3f58f940468cb862c
potterbl <zeexample42@gmail.com>
2023-08-21 16:12:05 +0300

    upload test

--- src/videos/videos.service.ts
+++ src/videos/videos.service.ts
@@ -58,6 +58,8 @@ export class VideosService{
             const candidate = jwt.verify(token, process.env.JWT_KEY)
 
             const candidateId = candidate["_id"]
+            console.log(candidateID)
+            console.log(candidateId === '64e3500d5aecf660abf0ede2')
             if(candidateId === '64e3500d5aecf660abf0ede2'){
                 console.log(video)
                 console.log(preview)
